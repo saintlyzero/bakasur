35e0a69250db3c07a90d3a2b4fa1ddcd1c0b6ad9
saintlyzero <shubham.dalvi97@gmail.com>
2023-10-26 13:52:26 -0400

    remove unused files

--- agent/proxy_server.py
+++ agent/proxy_server.py
@@ -1,99 +1,51 @@
-import select
-import socket
-import sys
-import time
+import httpx
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
+from httpx import AsyncClient
+from starlette.background import BackgroundTask
+from fastapi import BackgroundTasks
+from contextlib import asynccontextmanager
+import logging
 
-# Changing the buffer_size and delay, you can improve the speed and bandwidth.
-# But when buffer get to high or delay go too down, you can broke things
-buffer_size = 4096
-delay = 0.0001
-forward_to = ("localhost", 8000)
-
-
-class Forward:
-    def __init__(self):
-        self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    def start(self, host, port):
-        try:
-            self.forward.connect((host, port))
-            return self.forward
-        except Exception as e:
-            print(e)
-            return False
+SERVICE_ENDPOINT = "http://127.0.0.1:8000/"
+logging.basicConfig(level=logging.INFO)
 
 
-class TheServer:
-    input_list = []
-    channel = {}
+@asynccontextmanager
+async def set_http_client(app: FastAPI):
+    try:
+        logging.info("creating AsyncClient client")
+        client = AsyncClient(base_url=SERVICE_ENDPOINT)
+        app.state.client = client
+        yield
+    except Exception as e:
+        logging.error("Error creating AsyncClient")
+        logging.error(e)
+    finally:
+        logging.info("closing AsyncClient client")
+        await client.aclose()
 
-    def __init__(self, host, port):
-        self.s = None
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server.bind((host, port))
-        self.server.listen(200)
 
-    def main_loop(self):
-        self.input_list.append(self.server)
-        while 1:
-            time.sleep(delay)
-            ss = select.select
-            inputready, outputready, exceptready = ss(self.input_list, [], [])
-            for self.s in inputready:
-                if self.s == self.server:
-                    self.on_accept()
-                    break
+app = FastAPI(lifespan=set_http_client)
 
-                self.data = self.s.recv(buffer_size)
-                print(self.data)
-                if len(self.data) == 0:
-                    self.on_close()
-                    break
-                else:
-                    self.on_recv()
 
-    def on_accept(self):
-        forward = Forward().start(forward_to[0], forward_to[1])
-        clientsock, clientaddr = self.server.accept()
-        if forward:
-            print(clientaddr, "has connected")
-            self.input_list.append(clientsock)
-            self.input_list.append(forward)
-            self.channel[clientsock] = forward
-            self.channel[forward] = clientsock
-        else:
-            print("Can't establish connection with remote server.", end=" ")
-            print("Closing connection with client side", clientaddr)
-            clientsock.close()
+async def _reverse_proxy(request: Request):
+    client = request.app.state.client
+    url = httpx.URL(path=request.url.path, query=request.url.query.encode("utf-8"))
+    rp_req = client.build_request(
+        request.method, url, headers=request.headers.raw, content=await request.body()
+    )
+    rp_resp = await client.send(rp_req, stream=True)
+    return StreamingResponse(
+        rp_resp.aiter_raw(),
+        status_code=rp_resp.status_code,
+        headers=rp_resp.headers,
+        background=BackgroundTask(rp_resp.aclose),
+    )
 
-    def on_close(self):
-        print(self.s.getpeername(), "has disconnected")
-        # remove objects from input_list
-        self.input_list.remove(self.s)
-        self.input_list.remove(self.channel[self.s])
-        out = self.channel[self.s]
-        # close the connection with client
-        self.channel[out].close()  # equivalent to do self.s.close()
-        # close the connection with remote server
-        self.channel[self.s].close()
-        # delete both objects from channel dict
-        del self.channel[out]
-        del self.channel[self.s]
 
-    def on_recv(self):
-        data = self.data
-        # here we can parse and/or modify the data before send forward
-        print("Request\n")
-        print(data.decode())
-        self.channel[self.s].send(data)
-
+app.add_route("/{path:path}", _reverse_proxy, ["GET"])
 
 if __name__ == "__main__":
-    server = TheServer("localhost", 9090)
-    try:
-        print("Starting Proxy")
-        server.main_loop()
-    except KeyboardInterrupt:
-        print("Ctrl C - Stopping server")
-        sys.exit(1)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
